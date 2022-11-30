606c7ff696d93281cdf7fbf39ed67fa7f3359e7c
sanjanpallerla <113688942+sanjanpallerla@users.noreply.github.com>
2022-11-30 22:41:11 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React from "react";
+import NavBar from "./component/navBar.js"
+
+function App() {
+  return (
+    <div className="App">
+     {/*  */}
+     <NavBar/>
+    </div>
+  );
+}
+
+export default App;
